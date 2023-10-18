6212a80c0a5c851dd4dbed62fd0a92a25a3b3511
bidane <lnbidane@gmail.com>
2023-10-18 19:12:19 +0200

    a

--- sgssi-lab1/hash.sh
+++ sgssi-lab1/hash.sh
@@ -2,7 +2,7 @@
 
 read -p "Introduce el directorio con los archivos cuyo hash deseas: " direct
 read -p "Introduce el hash: " hash
-read -p "Introduce el nombre/nombre en común de los archivos: " nom
+
 cd $direct
 
 archivo=$(md5sum * | grep $hash)
